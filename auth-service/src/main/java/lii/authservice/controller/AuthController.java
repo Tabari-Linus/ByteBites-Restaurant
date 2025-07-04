b3641214a3c1438aaf2dcffcbb1530325f7cb016
Mr Lii <leetamparega@gmail.com>
2025-07-04 19:44:56 +0000

    docs: create authcontroller for registration and login

---
+++ auth-service/src/main/java/lii/authservice/controller/AuthController.java
@@ -0,0 +1,54 @@
+package lii.authservice.controller;
+
+import lii.authservice.domain.User;
+import lii.authservice.dto.AuthResponse;
+import lii.authservice.dto.LoginRequest;
+import lii.authservice.dto.RegisterRequest;
+import lii.authservice.repository.UserRepository;
+import lii.authservice.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest request) {
+        if (userRepository.findByEmail(request.email()).isPresent()) {
+            return ResponseEntity.badRequest().body("Error: Email is already in use!");
+        }
+        User user = new User();
+        user.setEmail(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setRole(User.Role.ROLE_CUSTOMER);
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        Authentication authenticationRequest =
+                UsernamePasswordAuthenticationToken.unauthenticated(request.email(), request.password());
+        Authentication authenticationResponse =
+                this.authenticationManager.authenticate(authenticationRequest);
+        User user = (User) authenticationResponse.getPrincipal();
+        String jwt = jwtService.generateToken(user);
+        return ResponseEntity.ok(new AuthResponse(jwt));
+    }
+}
