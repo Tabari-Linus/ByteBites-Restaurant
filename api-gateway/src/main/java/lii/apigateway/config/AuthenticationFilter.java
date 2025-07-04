d6bc55615e9dae248ca1e7fea0f1e312cd1bffbe
Mr Lii <leetamparega@gmail.com>
2025-07-04 19:36:26 +0000

    config: configure gateway for routes

---
+++ api-gateway/src/main/java/lii/apigateway/config/AuthenticationFilter.java
@@ -0,0 +1,87 @@
+package lii.apigateway.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Component
+@Slf4j
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private static final List<String> UNSECURED_PATHS = List.of(
+            "/auth/register",
+            "/auth/login"
+    );
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String path = exchange.getRequest().getURI().getPath();
+            if (UNSECURED_PATHS.stream().anyMatch(path::startsWith)) {
+                return chain.filter(exchange);
+            }
+
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            if (!headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, HttpStatus.UNAUTHORIZED);
+            }
+
+            String authHeader = headers.getFirst(HttpHeaders.AUTHORIZATION);
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                return onError(exchange, HttpStatus.UNAUTHORIZED);
+            }
+
+            String token = authHeader.substring(7);
+            try {
+                validateToken(token);
+                Logger.getLogger(AuthenticationFilter.class.getName()).info("Token is valid");
+            } catch (Exception e) {
+                log.error("Error validating token", e);
+                return onError(exchange, HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
+        exchange.getResponse().setStatusCode(status);
+        return exchange.getResponse().setComplete();
+    }
+
+    private void validateToken(String token) {
+        Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token);
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secret.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public static class Config {
+
+    }
+}
---
+++ api-gateway/src/main/java/lii/apigateway/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package lii.apigateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    private final AuthenticationFilter authenticationFilter;
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r.path("/auth/**")
+                        .filters( f -> f
+                                .filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
+                                .stripPrefix(1))
+                        .uri("lb://auth-service"))
+                .build();
+    }
+}
