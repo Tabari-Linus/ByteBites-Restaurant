54a71c8f0128d4695e245420cb36b0cc034d23d7
Mr Lii <leetamparega@gmail.com>
2025-07-05 02:23:54 +0000

    docs: create Restaurant repository

---
+++ restaurant-service/src/main/java/lii/restaurantservice/repository/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package lii.restaurantservice.repository;
+
+import lii.restaurantservice.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {}
