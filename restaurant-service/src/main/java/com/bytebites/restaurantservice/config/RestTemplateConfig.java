0fc13216c10165e7c672770007d167f9c6253996
Mr Lii <leetamparega@gmail.com>
2025-07-08 14:28:17 +0000

    docs: create RestTemplateConfig

---
+++ restaurant-service/src/main/java/com/bytebites/restaurantservice/config/RestTemplateConfig.java
@@ -0,0 +1,54 @@
+package com.bytebites.restaurantservice.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(new RestTemplateHeaderModifierInterceptor()));
+        return restTemplate;
+    }
+
+    public static class RestTemplateHeaderModifierInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body, 
+                ClientHttpRequestExecution execution) throws IOException {
+            
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+            if (requestAttributes instanceof ServletRequestAttributes) {
+                HttpServletRequest currentRequest = 
+                    ((ServletRequestAttributes) requestAttributes).getRequest();
+                
+                
+                String userId = currentRequest.getHeader("X-User-Id");
+                String userRoles = currentRequest.getHeader("X-User-Roles");
+                
+                if (userId != null) {
+                    request.getHeaders().set("X-User-Id", userId);
+                }
+                if (userRoles != null) {
+                    request.getHeaders().set("X-User-Roles", userRoles);
+                }
+            }
+            
+            return execution.execute(request, body);
+        }
+    }
+}
