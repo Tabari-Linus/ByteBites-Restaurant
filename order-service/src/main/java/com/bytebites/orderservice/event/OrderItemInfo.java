dd95ac37545bca90c4ee35e3f7ce6aa5567bc8f9
Mr Lii <leetamparega@gmail.com>
2025-07-09 09:40:12 +0000

    docs: create OrderItemInfo

---
+++ order-service/src/main/java/com/bytebites/orderservice/event/OrderItemInfo.java
@@ -0,0 +1,14 @@
+package com.bytebites.orderservice.event;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record OrderItemInfo(
+        @JsonProperty("menuItemId") UUID menuItemId,
+        @JsonProperty("menuItemName") String menuItemName,
+        @JsonProperty("unitPrice") BigDecimal unitPrice,
+        @JsonProperty("quantity") Integer quantity,
+        @JsonProperty("subtotal") BigDecimal subtotal
+) {}
