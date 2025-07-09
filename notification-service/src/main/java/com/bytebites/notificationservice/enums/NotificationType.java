1f12c97fda8d017cb314a1122a19a16287272478
Mr Lii <leetamparega@gmail.com>
2025-07-09 09:41:43 +0000

    docs: create NotificationType

---
+++ notification-service/src/main/java/com/bytebites/notificationservice/enums/NotificationType.java
@@ -0,0 +1,11 @@
+package com.bytebites.notificationservice.enums;
+
+public enum NotificationType {
+    ORDER_PLACED_CUSTOMER,
+    ORDER_PLACED_RESTAURANT,
+    ORDER_STATUS_CHANGED,
+    ORDER_DELIVERED,
+    ORDER_CANCELLED,
+    RESTAURANT_CREATED,
+    RESTAURANT_APPROVED
+}
