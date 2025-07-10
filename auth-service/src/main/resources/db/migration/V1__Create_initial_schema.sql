e2cfc548be9d06d80003eed124dacc15189e8515
Mr Lii <leetamparega@gmail.com>
2025-07-10 14:33:15 +0000

    config: add automatic default admin creation

--- auth-service/src/main/resources/db/migration/V1__Create_initial_schema.sql
+++ auth-service/src/main/resources/db/migration/V1__Create_initial_schema.sql
@@ -37,3 +37,26 @@ INSERT INTO roles (id, name) VALUES
                                  (gen_random_uuid(), 'ROLE_CUSTOMER'),
                                  (gen_random_uuid(), 'ROLE_RESTAURANT_OWNER'),
                                  (gen_random_uuid(), 'ROLE_ADMIN');
+
+
+INSERT INTO users (email, password, first_name, last_name, provider, enabled, created_at)
+VALUES (
+           'admin@gmail.com',
+           '$2a$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewfOUNHJdtNdOPf6',
+           'Admin',
+           'Lii',
+           'LOCAL',
+           true,
+           CURRENT_TIMESTAMP
+       );
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u, roles r
+WHERE u.email = 'admin@gmail.com'
+  AND r.name = 'ROLE_ADMIN';
+
+
+CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens(user_id);
+CREATE INDEX idx_refresh_tokens_token ON refresh_tokens(token);
