491f7962b36d4ba8505b1768cede81227e5101d0
Mr Lii <leetamparega@gmail.com>
2025-07-11 21:03:27 +0000

    refact: update UnauthorizedOperationException

--- restaurant-service/src/main/java/com/bytebites/restaurantservice/exception/UnauthorizedOperationException.java
+++ restaurant-service/src/main/java/com/bytebites/restaurantservice/exception/UnauthorizedOperationException.java
@@ -1,7 +1,14 @@
 package com.bytebites.restaurantservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedOperationException extends RuntimeException {
     public UnauthorizedOperationException(String message) {
         super(message);
+    }
+    public UnauthorizedOperationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
